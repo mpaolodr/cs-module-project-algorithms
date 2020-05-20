1e1887c8fae19ac3df755cacb6246465d51e044c
mpaolodr <mpaolodr_1504@yahoo.com>
2020-05-20 15:16:01 -0400

    moving_zeroes solution added and passing

--- moving_zeroes/moving_zeroes.py
+++ moving_zeroes/moving_zeroes.py
@@ -2,10 +2,17 @@
 Input: a List of integers
 Returns: a List of integers
 '''
+
+
 def moving_zeroes(arr):
     # Your code here
 
-    pass
+    for i in arr:
+        if i == 0:
+            arr.remove(0)
+            arr.append(0)
+
+    return arr
 
 
 if __name__ == '__main__':
